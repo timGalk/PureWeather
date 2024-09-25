dbc8c9c66f77623b050ea9431fbb7b0046dbbf9f
timGalk <tivron@outlook.com>
2024-09-25 15:41:37 +0200

    added App.py

---
+++ App.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import requests
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from geopy.geocoder import Nominatim 
+
+root = tk.Tk()
+root_tilte = "Pure Weather"
